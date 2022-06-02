696499ca5c51ce9b8e8cf2915e466a6e9bb31975
SunnyDay <1246771571@qq.com>
2022-06-02 11:51:38 +0800

    UserCenter 配置

---
+++ UserCenter/src/main/java/com/user/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.user.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.user.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

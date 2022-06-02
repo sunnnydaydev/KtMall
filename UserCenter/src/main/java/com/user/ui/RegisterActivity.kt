6913967d10327dbbf168abc79b3e90f3c16c9ed5
SunnyDay <1246771571@qq.com>
2022-06-02 18:31:29 +0800

    三方库引入

--- UserCenter/src/main/java/com/user/ui/RegisterActivity.kt
+++ UserCenter/src/main/java/com/user/ui/RegisterActivity.kt
@@ -2,11 +2,16 @@ package com.user.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.user.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btnRegister.setOnClickListener {
+            Toast.makeText(applicationContext,"注册",Toast.LENGTH_LONG).show()
+        }
     }
 }
